9183c994510da49c2014de69f12a2ac10ca0a282
Amanbharti <141431731+amanbharti72@users.noreply.github.com>
2024-01-29 15:56:55 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,13 @@
+const accountId = 145335;
+let accountEmail = "Aman@gmail.com";
+var accountPassword ="123456789";
+accountCity ="Jaipur";
+let accountState;
+
+//accountId = 2; //not alloweds
+
+accountEmail = "bharti@gmail.com";
+accountPassword = "1222222";
+accountCity = "Bihar";
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.log(accountId);
